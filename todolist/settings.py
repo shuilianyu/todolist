75df122c4815f4e1a9b39f4b1738f36bb73bcb93
shuilianyu <13783548908@163.com>
2018-08-26 21:45:47 +0800

    database settings

--- todolist/settings.py
+++ todolist/settings.py
@@ -74,8 +74,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'todolist',
-        'USER': 'yangfubo',
-        'PASSWORD': 'bo960926',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
